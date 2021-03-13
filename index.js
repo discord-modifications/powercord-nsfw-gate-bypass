04f82660f3b63f468615982c54cbd9c1630426b7
eternal <73202350+slow@users.noreply.github.com>
2021-03-13 14:25:44 +0000

    fix it

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const { Plugin } = require('powercord/entities');
 const { waitFor } = require('powercord/util');
 const { getModule } = require('powercord/webpack');
-const { getCurrentUser } = getModule(['getCurrentUser'], false);
+const { inject, uninject } = require('powercord/injector');
+const UserModule = getModule(['getCurrentUser'], false);
 
 module.exports = class NSFWBypass extends Plugin {
    async startPlugin() {
       await waitFor('.usernameContainer-1fp4nu');
-      let user = getCurrentUser();
-      user._nsfwAllowed = user.nsfwAllowed;
-      user.nsfwAllowed = true;
+      inject('nsfw-gate-bypass', UserModule, 'getCurrentUser', (_, res) => {
+         if (res.nsfwAllowed == false) res.nsfwAllowed = true;
+         return res;
+      });
    }
 
    pluginWillUnload() {
-      let user = getCurrentUser();
-      user.nsfwAllowed = user._nsfwAllowed;
+      uninject('nsfw-gate-bypass');
    }
 };
